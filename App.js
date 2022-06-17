11f9d546ad115197ae39d9da1b560bf4d4993206
ramansaini95 <deep65399@gmail.com>
2022-06-17 18:18:28 +0530

    counter app with redux

--- App.js
+++ App.js
@@ -3,110 +3,61 @@
  * https://github.com/facebook/react-native
  *
  * @format
- * @flow strict-local
+ * @flow
  */
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+ import React, { Fragment, Component } from 'react';
+ import {
+   SafeAreaView,
+   StyleSheet,
+   View,
+   Button,
+   Text
+ } from 'react-native';
+ 
+ 
+ class App extends Component {
+   state = { count: 0 };
+   decrementCount() {
+     let { count } = this.state;
+     count= count-1;
+     this.setState({
+       count
+     })
+   }
+   incrementCount() {
+     let { count } = this.state;
+     count++;
+     this.setState({
+       count
+     })
+   }
+   render() {
+     const { count } = this.state;
+     return (
+      <SafeAreaView  style={{flex:1,}}>
+       <View styles={styles.container}>
+         <Button
+           title="increment"
+           onPress={() => this.incrementCount()}
+         />
+         <Text>{count}</Text>
+         <Button
+           title="decrement"
+           onPress={() => this.decrementCount()}
+         />
+       </View>
+       </SafeAreaView>
+     );
+   }
+ };
+ 
+ const styles = StyleSheet.create({
+   container: {
+     flex: 1,
+     justifyContent: 'center',
+     alignItems: 'center'
+   }
+ });
+ 
+ export default App;
---
+++ src/actions/counts.js
@@ -0,0 +1,7 @@
+import { COUNTER_CHANGE } from '../constants/index';
+export function changeCount(count) {
+return {
+type: COUNTER_CHANGE,
+payload: count
+}
+}
